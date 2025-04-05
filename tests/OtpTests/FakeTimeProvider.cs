f1ed77008a13313b4dbde3ca5b1eabe3921abd17
Ole <thomasolesen0505@hotmail.com>
2025-04-05 11:59:24 +0200

    Add testing for PostgresOtpRepository
    Tags: test, OtpRepositoryTests, OtpTests

---
+++ tests/OtpTests/FakeTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿namespace OtpTests;
+
+public class FakeTimeProvider(DateTimeOffset startDateTime) : TimeProvider
+{
+    private DateTimeOffset _currentTime = startDateTime;
+
+    public void Advance(TimeSpan timeSpan)
+    {
+        _currentTime = _currentTime.Add(timeSpan);
+    }
+
+    public override DateTimeOffset GetUtcNow() => _currentTime;
+}
