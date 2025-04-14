edfc7794fa6c02da89a682f888344f4e961d0fac
Tralalula <qenzoz@gmail.com>
2025-04-14 21:44:51 +0200

    Tilføj TestDataSeeder med SeedUserAsync-metode
    Tilføjer en `TestDataSeeder` hjælpe-klasse der indeholder extension
    metoder til at hjælpe med at tilføje data til databasen.
    
    Der er et eksempel med en `SeedUserAsync`-metode som gør det nemt at
    oprette `User` testdata.
    
    Det har til formål at reducere mængden af boilerplate kode i `Arrange`-
    fasen af tests, der kræver at eksempelvis at en eksisterende bruger
    eksister, f.eks. pga. foreing key constraints.
    
    F.eks. kan man bruge `SeedUserAsync` på følgende måde:
    ```csharp
    _dbContext.SeedUserAsync();
    ```
    
    Her tilføjes der en bruger til databasen med email "peter@test.dk".
    
    Kan også:
    ```csharp
    _dbContext.SeedUserAsync("kakao@mælk.dk");
    ```
    
    Tags: testing, seed, test data, boilerplate, user

---
+++ server/tests/Infrastructure.Data.Tests/TestDataSeeder.cs
@@ -0,0 +1,22 @@
+using Core.Entities;
+using Infrastructure.Data.Postgres;
+
+namespace Infrastructure.Data.Tests;
+
+public static class TestDataSeeder
+{
+    public static async Task<User> SeedUserAsync(this PostgresDbContext context,
+                                                 TimeProvider timeProvider,
+                                                 string email = "peter@test.dk")
+    {
+        var user = new User
+        {
+            Email = email,
+            CreatedAt = timeProvider.GetUtcNow().UtcDateTime
+        };
+        context.Users.Add(user);
+        await context.SaveChangesAsync();
+        context.ChangeTracker.Clear();
+        return user;
+    }
+}
