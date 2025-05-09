2924bf9cbe1a83194f25ef8cf859f45a32ae4825
jesnie02 <jesnie02@easv365.dk>
2025-05-09 19:42:55 +0200

    Toast
    Why: Jeg er gået med sonner som er en blanding mellem shadcn og react-tostify.
    
    How:
    den fungere som react-tostify
    import { toast } from 'sonner';
    toast.succes('besked');
    
    mere info ses på https://sonner.emilkowal.ski/toast#promise
    
    Tags: toast, sonner

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import { Home, InitiateLoginPage, CompleteLoginPage } from "./pages";
 import { RequireAuth } from "./components/RequireAuth";
 import { useAuth } from "./hooks/useAuth";
 import { AuthContext } from './AuthContext';
+import { Toaster} from 'sonner';
 
 export default function App() {
     const auth = useAuth();
     
     return (
         <AuthContext.Provider value={auth}>
+            <Toaster richColors position="top-right" />
             <Routes>
                 <Route path={AppRoutes.home} element={<RequireAuth accessLevel={AccessLevel.User} element={<Home />}/>}/>
                 <Route path={AppRoutes.login} element={<InitiateLoginPage />} />
