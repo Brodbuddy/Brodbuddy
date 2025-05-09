51087d813ebb14a9b107421c283e9a2e6fe69a50
Tralalula <qenzoz@gmail.com>
2025-05-09 17:44:44 +0200

    Refactor måde at generate WS client spec
    Kan nu skrive `dotnet run --generate-ws-client` for at generate WS
    client API fil.

--- server/Api.Websocket/Extensions.cs
+++ server/Api.Websocket/Extensions.cs
@@ -1,6 +1,5 @@
 using Api.Websocket.Auth;
 using Api.Websocket.ExceptionHandler;
-using Api.Websocket.Spec;
 using Brodbuddy.WebSocket.Auth;
 using Brodbuddy.WebSocket.Core;
 using Microsoft.AspNetCore.Builder;
@@ -14,28 +13,13 @@ public static class Extensions
     {
         services.AddWebSocketHandlers(typeof(FleckWebSocketServer).Assembly);
         services.AddHostedService<FleckWebSocketServer>();
-        
+
         services.AddSingleton<IWebSocketAuthHandler, JwtWebSocketAuthHandler>();
         services.AddSingleton<IWebSocketExceptionHandler, GlobalWebsocketExceptionHandler>();
-        
-        services.GenerateClientApi();
-        
+
         return services;
     }
 
-    private static IServiceCollection GenerateClientApi(this IServiceCollection services)
-    {
-        var baseDir = Directory.GetCurrentDirectory();
-        var templatesDir = Path.Combine(baseDir, "../Api.Websocket/Spec");
-        var outputDir = Path.Combine(baseDir, "../../client/src/api");
-        Directory.CreateDirectory(outputDir);
-        
-        var spec = SpecGenerator.GenerateSpec(typeof(FleckWebSocketServer).Assembly, services.BuildServiceProvider());
-        TypeScriptGenerator.Generate(spec, templatesDir, outputDir);
-        
-        return services;
-    }
-    
     public static WebApplication ConfigureWebsocketApi(this WebApplication app)
     {
         var dispatcher = app.Services.GetRequiredService<WebSocketDispatcher>();
