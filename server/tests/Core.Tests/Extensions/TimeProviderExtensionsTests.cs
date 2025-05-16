5ea5b527beac3d35baec18b63f86a62e396fc212
Tralalula <qenzoz@gmail.com>
2025-05-16 16:13:19 +0200

    Tilføj manglede test for Core

--- server/tests/Core.Tests/Extensions/TimeProviderExtensionsTests.cs
+++ server/tests/Core.Tests/Extensions/TimeProviderExtensionsTests.cs
@@ -25,4 +25,24 @@ public class TimeProviderExtensionsTests
         actualTomorrow.ShouldBe(expectedTomorrow); 
         (actualTomorrow - actualNow).ShouldBe(TimeSpan.FromDays(1));
     }
+    
+    [Fact]
+    public void Yesterday_ShouldReturnDateTimeExactlyOneDayBeforeNow()
+    {
+        // Arrange
+        var startTime = new DateTimeOffset(2024, 5, 15, 10, 30, 0, TimeSpan.Zero);
+        var timeProvider = new FakeTimeProvider(startTime);
+
+        var expectedNow = startTime.UtcDateTime;
+        var expectedYesterday = expectedNow.AddDays(-1);
+
+        // Act
+        var actualNow = timeProvider.Now();
+        var actualYesterday = timeProvider.Yesterday();
+
+        // Assert
+        actualNow.ShouldBe(expectedNow);
+        actualYesterday.ShouldBe(expectedYesterday);
+        (actualNow - actualYesterday).ShouldBe(TimeSpan.FromDays(1)); 
+    }
 }
